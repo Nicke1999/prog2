90f1783a2b2f31ac94a22b5c14ebeaa1be7e2dd9
Niklas Eckert Elfving <niec5846@arrhenius.it.uu.se>
2021-10-12 15:08:35 +0200

    integer.cpp done

--- MA4_files/integer.cpp
+++ MA4_files/integer.cpp
@@ -6,6 +6,7 @@ class Integer{
 		Integer(int);
 		int get();
 		void set(int);
+		int Fib(int);
 	private:
 		int val;
 	};
@@ -18,6 +19,15 @@ int Integer::get(){
 	return val;
 	}
  
+int Integer::Fib(int n){
+	if((n==1)||(n==0)) { 
+	return(n);
+	}else {
+	return(Fib(n-1)+Fib(n-2));
+   }
+}
+
+
 void Integer::set(int n){
 	val = n;
 	}
@@ -26,7 +36,8 @@ void Integer::set(int n){
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
-	void Integer_set(Integer* integer, int n) {integer->set(n);}
+	int Integer_Fib(Integer* integer, int n) {return integer->Fib(n);} 
+  	void Integer_set(Integer* integer, int n) {integer->set(n);}
 	void Integer_delete(Integer* integer){
 		if (integer){
 			delete integer;
